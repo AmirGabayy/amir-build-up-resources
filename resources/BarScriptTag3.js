8febd4b6402aa51b4ac142015c4a4a27b3841a72
AmirGabayy <49371295+AmirGabayy@users.noreply.github.com>
2022-11-15 11:43:18 +0200

    3

--- resources/BarScriptTag3.js
+++ resources/BarScriptTag3.js
@@ -1,6 +1,6 @@
 const method = "GETss";
 const hostScheme = "https";
-const hostName = "https://590b-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
+const hostName = "590b-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
 const endPoint = "/bars/amir-test-store-app.myshopify.com";
 
 const url = hostScheme + "://" + hostName + endPoint + "/";
