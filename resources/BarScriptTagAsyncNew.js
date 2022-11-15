219f783edd0ac97df7c3ddb715a95f6fc5e41086
AmirGabayy <49371295+AmirGabayy@users.noreply.github.com>
2022-11-15 11:19:01 +0200

    ngrok update

--- resources/BarScriptTagAsyncNew.js
+++ resources/BarScriptTagAsyncNew.js
@@ -1,6 +1,6 @@
 const method = "GETss";
 const hostScheme = "https";
-const hostName = "06da-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
+const hostName = "https://ebe8-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
 const endPoint = "/bars/amir-test-store-app.myshopify.com";
 
 const url = hostScheme + "://" + hostName + endPoint + "/";
