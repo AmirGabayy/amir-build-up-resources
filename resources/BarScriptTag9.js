0311da80cd38f3a58825c1eadf598aad21f65fbd
AmirGabayy <49371295+AmirGabayy@users.noreply.github.com>
2022-11-15 14:04:36 +0200

    node activation

--- resources/BarScriptTag9.js
+++ resources/BarScriptTag9.js
@@ -1,19 +1,44 @@
+import fetch from "node-fetch";
+
 const method = "GET";
 const hostScheme = "https";
 const hostName = "590b-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
-const endPoint = "/bars/retrieve/amir-test-store-app.myshopify.com";
+const endPoint = "bars/retrieve";
+const storeName = "amir-test-store-app.myshopify.com";
 
-const url = hostScheme + "://" + hostName + endPoint + "/";
+const url =
+  hostScheme + "://" + hostName + "/" + endPoint + "/" + storeName + "/";
 
 async function fetchProducts() {
   console.log("sending requst to url: " + url);
 
   const response = await fetch(url, {
     method,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      // Accept: "text/html",
+      "Content-Type": "application/json",
+    },
   });
 
-  console.log("response from request: " + response.json());
+  if (response.ok) {
+    const products = await response.json();
+
+    return products;
+
+    // products.forEach(({ mainImageSrc }) => {
+    //   console.log(mainImageSrc);
+    // });
+
+    // console.log("response from request: " + JSON.stringify(products));
+  } else {
+    return undefined;
+  }
 }
 
-fetchProducts();
+fetchProducts().then((products) => {
+  if (products) {
+    products.forEach(({ mainImageSrc }) => {
+      console.log(mainImageSrc);
+    });
+  }
+});
