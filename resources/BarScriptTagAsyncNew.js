02096834661b8ba20e0a6001d68b0b31c00c061e
AmirGabayy <49371295+AmirGabayy@users.noreply.github.com>
2022-11-15 11:12:40 +0200

    init

---
+++ resources/BarScriptTagAsyncNew.js
@@ -0,0 +1,19 @@
+const method = "GETs";
+const hostScheme = "https";
+const hostName = "06da-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
+const endPoint = "/bars/amir-test-store-app.myshopify.com";
+
+const url = hostScheme + "://" + hostName + endPoint + "/";
+
+async function fetchProducts() {
+  console.log("sending requst to url: " + url);
+
+  const response = await fetch(url, {
+    method,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  console.log("response from request: " + JSON.stringify(response));
+}
+
+fetchProducts();
