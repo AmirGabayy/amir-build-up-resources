5f92f4b5626ba739171340fc50b0d09861418399
AmirGabayy <49371295+AmirGabayy@users.noreply.github.com>
2022-11-16 13:34:16 +0200

    b3

--- resources/BScriptTag3..js
+++ resources/BScriptTag3..js
@@ -1,5 +1,20 @@
 // import fetch from "node-fetch";
 
+const method = "GET";
+const hostScheme = "https";
+const hostName = "590b-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
+const endPoint = "bars/retrieve";
+const storeName = "amir-test-store-app.myshopify.com";
+const EMPTY_PRODUCT_URL =
+  "https://static.vecteezy.com/system/resources/previews/002/916/029/original/realistic-white-round-podium-illuminated-by-spotlight-empty-winner-stage-product-pedestal-exhibition-platform-vector.jpg";
+const PRODUCT_WIDTH = 50;
+const PRODUCT_HEIGHT = 50;
+const CSS_URL =
+  "https://cdn.statically.io/gh/AmirGabayy/amir-build-up-resources/main/resources/style2.css";
+
+const url =
+  hostScheme + "://" + hostName + "/" + endPoint + "/" + storeName + "/";
+
 window.onload = function () {
   let head = document.getElementsByTagName("head")[0];
 
@@ -23,8 +38,7 @@ window.onload = function () {
     link.id = cssId;
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href =
-      "https://cdn.statically.io/gh/AmirGabayy/amir-build-up-resources/main/resources/style1.css";
+    link.href = CSS_URL;
     link.media = "all";
     head.appendChild(link);
   }
@@ -52,25 +66,11 @@ async function checkCss() {
     link.id = cssId;
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href =
-      "https://cdn.statically.io/gh/AmirGabayy/amir-build-up-resources/main/resources/style1.css";
+    link.href = CSS_URL;
     link.media = "all";
     head.appendChild(link);
   }
 }
-
-const method = "GET";
-const hostScheme = "https";
-const hostName = "590b-2a0d-6fc0-2803-6000-c002-c60f-e32f-913.ngrok.io";
-const endPoint = "bars/retrieve";
-const storeName = "amir-test-store-app.myshopify.com";
-const EMPTY_PRODUCT_URL =
-  "https://static.vecteezy.com/system/resources/previews/002/916/029/original/realistic-white-round-podium-illuminated-by-spotlight-empty-winner-stage-product-pedestal-exhibition-platform-vector.jpg";
-const PRODUCT_WIDTH = 50;
-const PRODUCT_HEIGHT = 50;
-
-const url =
-  hostScheme + "://" + hostName + "/" + endPoint + "/" + storeName + "/";
 
 async function fetchProducts() {
   // console.log("sending requst to url: " + url);
